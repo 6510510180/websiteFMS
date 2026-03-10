d2d556452021d28d52d3a9a1238a7322a1b5ea28
6510510180 <6510510180@psu.ac.th>
2026-03-10 13:43:27 +0700

    update

--- backend/server.js
+++ backend/server.js
@@ -2120,6 +2120,6 @@ app.get("*", (req, res) => {
 // ============================================================
 //  Start Server
 // ============================================================
-app.listen(PORT, () => {
+app.listen(PORT, '0.0.0.0', () => {
   console.log(`🚀 FMS backend listening on port ${PORT}`);
 });
